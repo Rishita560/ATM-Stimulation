537cf3e669e23d3a6fedc1e88d6440d694399551
Rishita560 <rishitasahu23@gmail.com>
2025-11-05 19:43:50 +0530

    Added code in Connn

--- src/atm/Connn.java
+++ src/atm/Connn.java
@@ -1,4 +1,16 @@
 package atm;
 
+import java.sql.*;
+
 public class Connn {
+    Connection connection;
+    Statement statement;
+    public Connn(){
+        try{
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bankSystem","root","AyushVish");
+            statement = connection.createStatement();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
