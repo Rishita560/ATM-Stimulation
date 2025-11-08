6f8ad3092062e3d9dbca1596f703aa3a3904f9d9
veedushi29 <veedushisahu2004@gmail.com>
2025-11-08 20:00:26 +0530

    modified code

--- src/atm/SignUp2.java
+++ src/atm/SignUp2.java
@@ -17,7 +17,7 @@ public class SignUp2 extends JFrame implements ActionListener {
 
     String formno;
 
-    SignUp2(String first) {
+    SignUp2(String formno ) {
         super("APPLICATION FORM");
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("bank.png"));
@@ -155,12 +155,12 @@ public class SignUp2 extends JFrame implements ActionListener {
         add(e2);
 
         JLabel l12 = new JLabel("Form No.");
-        l12.setFont(new Font("Rale way", Font.BOLD, 18));
+        l12.setFont(new Font("Rale way", Font.BOLD, 14));
         l12.setBounds(700, 10, 60, 30);
         add(l12);
 
         JLabel l13 = new JLabel(formno);
-        l13.setFont(new Font("Rale way", Font.BOLD, 18));
+        l13.setFont(new Font("Rale way", Font.BOLD, 14));
         l13.setBounds(760, 10, 100, 30);
         add(l13);
 
